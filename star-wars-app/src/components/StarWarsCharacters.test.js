c212e52f96d908edb806a8436c7b5266bad0413c
CAM603 <cameronhawley@comcast.net>
2020-01-27 18:27:26 -0500

    stuck on tests

--- star-wars-app/src/components/StarWarsCharacters.test.js
+++ star-wars-app/src/components/StarWarsCharacters.test.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { render, fireEvent, wait } from '@testing-library/react';
-import { getData as mockGetData } from '../api';
+import { getData as mockGetData } from '../api/getData';
 import StarWarsCharacters from './StarWarsCharacters';
 
 jest.mock('../api')
 
 test('Something happens', async () => {
+    // mockGetData.mockResolvedValueOnce([{
+    //     name: "Luke Skywalker",
+    //     height: "172",
+    //     mass: "77",
+    //     hair_color: "blond",
+    //     skin_color: "fair",
+    //     eye_color: "blue",
+    //     birth_year: "19BBY",
+    //     gender: "male",
+    //     homeworld: "https://swapi.co/api/planets/1/"
+    // }])
 
     const { getByText } = render(<StarWarsCharacters/>);
 
